c3356de7e3b4dde29a5b0cf88cb8afd80baac069
pepperoni <josef.hlavaty@digital.cz>
2025-09-23 21:52:27 +0200

    add profiler, edit test controller

--- project/src/UI/Http/TestController.php
+++ project/src/UI/Http/TestController.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace App\UI\Http;
 
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -12,15 +11,9 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/test')]
 final class TestController extends AbstractController
 {
-    public function __construct(
-        private readonly EntityManagerInterface $entityManager,
-    ) {
-    }
 
     public function __invoke(): Response
     {
-
-
-        return new Response('Test');
+        return $this->render('test.html.twig');
     }
 }

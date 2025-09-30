1756dc7ddb2d0baf01c08eaf94ce824963635cc3
pepperoni <josef.hlavaty@digital.cz>
2025-09-30 22:29:37 +0200

    add flash messages

--- project/src/UI/Http/QuizSession/AnswerQuestionAction.php
+++ project/src/UI/Http/QuizSession/AnswerQuestionAction.php
@@ -13,17 +13,23 @@ use Symfony\Component\Routing\Attribute\Route;
 #[Route('/quiz-session/{quizSession}/answer-question', name: 'app_quiz_session_answer_question', methods: ['POST'])]
 final class AnswerQuestionAction extends AbstractController
 {
+
+
     public function __invoke(QuizSession $quizSession, Request $request): Response
     {
         $answer = $request->get('answer');
 
-        $result = $quizSession->getCurrentQuestion()->getAnswer() === $answer;
+        $correctAnswer = $quizSession->getCurrentQuestion()->getAnswer();
+        $result = $correctAnswer === $answer;
 
-        $eessage = $result ? 'Správně!' : 'Nesprávně!';
+        $type = $result ? 'success' : 'danger';
+        $message = $result
+            ? sprintf('Správně, odpověď skutečně byla "%s".', $correctAnswer)
+            : sprintf('Špatně, správná odpověď byla "%s".', $correctAnswer);
 
         $this->addFlash(
-            'success',
-            $eessage,
+            $type,
+            $message,
         );
 
         return $this->redirectToRoute(

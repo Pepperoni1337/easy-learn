b06ce8b44b3ba15805c055483d633a210e150cf8
pepperoni <josef.hlavaty@digital.cz>
2025-09-30 22:20:27 +0200

    mvp answering questions

--- project/src/UI/Http/QuizSession/AnswerQuestionAction.php
+++ project/src/UI/Http/QuizSession/AnswerQuestionAction.php
@@ -6,13 +6,31 @@ namespace App\UI\Http\QuizSession;
 
 use App\Core\QuizSession\Model\QuizSession;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/quiz-session/{quizSession}/answer-question', name: 'app_quiz_session_answer_question', methods: ['POST'])]
 final class AnswerQuestionAction extends AbstractController
 {
-    public function __invoke(QuizSession $quizSession)
+    public function __invoke(QuizSession $quizSession, Request $request): Response
     {
-        // TODO: Implement __invoke() method.
+        $answer = $request->get('answer');
+
+        $result = $quizSession->getCurrentQuestion()->getAnswer() === $answer;
+
+        $eessage = $result ? 'Správně!' : 'Nesprávně!';
+
+        $this->addFlash(
+            'success',
+            $eessage,
+        );
+
+        return $this->redirectToRoute(
+            'app_quiz_session_get_question',
+            [
+                'quizSession' => $quizSession->getId(),
+            ],
+        );
     }
 }

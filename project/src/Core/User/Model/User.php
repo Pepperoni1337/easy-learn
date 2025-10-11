e34f054f72f6ddedd68b8278835c41f3a70f90b0
pepperoni <josef.hlavaty@digital.cz>
2025-10-11 20:17:07 +0200

    edit user entity

--- project/src/Core/User/Model/User.php
+++ project/src/Core/User/Model/User.php
@@ -8,19 +8,42 @@ use App\Core\Shared\Model\Entity;
 use App\Core\Shared\Model\EntityTrait;
 use App\Core\Shared\Model\Id;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity]
-class User implements Entity
+class User implements Entity, UserInterface, PasswordAuthenticatedUserInterface
 {
     use EntityTrait;
+
+    #[ORM\Column(unique: true)]
+    private string $email;
 
     public function __construct()
     {
         $this->id = Id::new();
     }
 
-    public function getEmail(): string
+    public function getRoles(): array
     {
-        return 'ahoj';
+        return [
+            'ROLE_USER',
+            'ROLE_ADMIN',
+        ];
+    }
+
+    public function eraseCredentials(): void
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function getUserIdentifier(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): ?string
+    {
+        return 'abc';
     }
 }

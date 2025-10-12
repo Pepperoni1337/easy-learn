882c96118bd1c0b3773bb7d802d83cda825a0ba1
pepperoni <josef.hlavaty@digital.cz>
2025-10-12 06:39:28 +0200

    fix template

--- project/src/UI/Http/DashboardAction.php
+++ project/src/UI/Http/DashboardAction.php
@@ -13,7 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/', name: 'app_index')]
+#[Route('/', name: 'app_dashboard')]
 final class DashboardAction extends AbstractController
 {
     use WithEntityManager;
